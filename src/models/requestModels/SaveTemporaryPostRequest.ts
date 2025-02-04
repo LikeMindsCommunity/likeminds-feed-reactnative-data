2d6b72b245a1a61b28189c0fe601b44d2ec21bfb
Usman salim <usman.salim@likeminds.community>
2025-02-04 16:50:27 +0530

    updated models

--- src/models/requestModels/SaveTemporaryPostRequest.ts
+++ src/models/requestModels/SaveTemporaryPostRequest.ts
@@ -1,5 +1,5 @@
-import { TemporaryPost } from "src/post/models/TemporaryPost";
+import { TempPost } from "src/post/models/TempPost";
 
 export interface SaveTemporaryPostRequest {
-    tempPost: TemporaryPost
+    tempPost: TempPost
 }
--- src/models/responseModels/GetTemporaryPostResponse.ts
+++ src/models/responseModels/GetTemporaryPostResponse.ts
@@ -1,5 +1,5 @@
-import { TemporaryPost } from "src/post/models/TemporaryPost";
+import { TempPost } from "src/post/models/TempPost";
 
 export interface GetTemporaryPostResponse {
-    tempPost?: TemporaryPost 
+    tempPost?: TempPost 
 }
